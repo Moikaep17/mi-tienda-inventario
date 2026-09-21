679068996d7849edc26e530d293525641a806af9
Moikaep17 <moisesedgardoperez@gmail.com>
2026-09-20 18:30:28 -0600

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
    CONFIGURACIÓN
    ===================================================== */
 
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxpDOWQIMwi-v_-lI2xiIUzqtP1j_ujOXT8SYn9K0ffqVCG2tSzxv5EBhE3B2kEvAAPNQ/exec";
+const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxpDOWQIMwi-v_-lI2xiIUzqtP1j_ujOXT8SYn9K0ffqVCG2tSzxv5EBhE3B2kEvAAPNQ/exec;
     "PEGA_AQUI_TU_URL_DE_APPS_SCRIPT";
 
 
